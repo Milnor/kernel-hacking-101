27c49a4e405619ff3383f873c9d66c1bb3fe5d90
kernel <kernel@localhost.localdomain>
2019-10-09 22:16:44 -0400

    starting to implement write functionality

--- 02_char_driver/reverse.c
+++ 02_char_driver/reverse.c
@@ -1,12 +1,30 @@
 #include <linux/module.h>
 #include <linux/kdev_t.h>   // dev_t type
 #include <linux/fs.h>       // chrdev registration
-#define MAJ_NUM   242       // MAJ, MIN device number
-#define MIN_NUM   1337
 
-MODULE_LICENSE("GPL");
+#define MAJ_NUM     242       // MAJ, MIN device number
+#define MIN_NUM     1337
+#define BUFF_SIZE   2048
+
+static ssize_t reverse_read(struct file *, char __user *,size_t, loff_t *);
+static ssize_t reverse_write(struct file *, const char __ user *, size_t, loff_t *);
 
 static dev_t device_number;
+
+typedef struct buffer
+{
+    char data[BUFF_SIZE],
+    int index;
+} buffer_t;
+
+static buffer_t char_buffer;
+
+static struct file_operations reverse_ops =
+{
+    .owner = THIS_MODULE,
+    .read = reverse_read,
+    .write = reverse_write,
+};
 
 static int __init load_driver(void)
 {
@@ -29,3 +47,4 @@ static void __exit unload_driver(void)
 
 module_init(load_driver);
 module_exit(unload_driver);
+MODULE_LICENSE("GPL");
